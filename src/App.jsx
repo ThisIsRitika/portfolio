8173ff0d03e2f546c004271e3565f43fad7f181a
ThisIsRitika <rbalhara2004@gmail.com>
2025-09-30 21:26:06 +0530

    save changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
@@ -7,12 +7,12 @@ function App() {
   return (
     <>
       <Toaster />
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
